35a4a37b8ebf3fae044cfb7825761987c10df6f0
Vee Li <michaelvli@yahoo.com>
2015-07-03 05:48:28 -0500

    UI Changes and Lightbox fix
    - Lightbox function was in wrong place - causing it to be called 2x
    (once by "Your Word" field and the other by "Story" textarea)
    - Modified "Your Word" field to display capital letters.  Note: data
    entered by user will still be stored as lowercase (if that's how they
    entered it) until the model is modified.

--- js/prototype/myintent_validation.js
+++ js/prototype/myintent_validation.js
@@ -1,4 +1,4 @@
-// customization - only allow uppercase chars, numbers, and &
+// customization - only allow letters, numbers, "&", and spaces
 Validation.add('validate-myintent-chars', 'Please use uppercase characters, numbers, or "&"', function(v) {
-    return Validation.get('IsEmpty').test(v) || /^[A-Z0-9&\x20]+$/.test(v)
-})
+    return Validation.get('IsEmpty').test(v) || /^[a-zA-Z0-9&\x20]+$/.test(v);
+});
