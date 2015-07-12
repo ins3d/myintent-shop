907a59de898bc577ca70363e332c05c69cd55063
Matt Truebe <matt@frequency.com>
2015-07-12 15:29:53 -0700

    Customer Validation Change

--- app/code/core/Mage/Customer/Model/Customer.php
+++ app/code/core/Mage/Customer/Model/Customer.php
@@ -839,8 +839,11 @@ class Mage_Customer_Model_Customer extends Mage_Core_Model_Abstract
             $errors[] = Mage::helper('customer')->__('The minimum password length is %s', 6);
         }
 
-        $confirmation = $this->getPasswordConfirmation();
-        if ($password != $confirmation) {
+        $confirmation = $this->getConfirmation();
+        $passwordconfirmation = $this->getPasswordConfirmation();
+        //if ($password != $confirmation) {
+        if (!(($password == $confirmation) ||
+            ($password == $passwordconfirmation))) {
             $errors[] = Mage::helper('customer')->__('Please make sure your passwords match.');
         }
 
