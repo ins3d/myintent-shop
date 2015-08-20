399b5451d9fd87eda838dc22ac295a486ca1a30a
Vee Li <michaelvli@yahoo.com>
2015-08-19 20:14:24 -0500

    Updated validation message

--- js/prototype/myintent_validation.js
+++ js/prototype/myintent_validation.js
@@ -1,4 +1,4 @@
 // customization - only allow letters, numbers, "&", and spaces
-Validation.add('validate-myintent-chars', 'Use A-Z capital letters, 0-9, or "&" only', function(v) {
+Validation.add('validate-myintent-chars', 'Use only A-Z capital letters, 0-9, and "&"', function(v) {
     return Validation.get('IsEmpty').test(v) || /^[A-Z0-9&\x20]+$/.test(v);
 });
